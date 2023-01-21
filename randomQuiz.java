603a362e75340ee51e43724fb120478924a4fda0
MagentaMagenta <119783546+FrinceNacion@users.noreply.github.com>
2023-01-21 13:08:39 +0800

    Update randomQuiz.java

--- randomQuiz.java
+++ randomQuiz.java
@@ -20,7 +20,7 @@ public class Quiz {
             "What is 18 x 19?",
             "What is 20 x 21?"
             };
-        int[] answerBank = {2 * 3,4 * 5,6 * 7,8 * 9,10 * 11,12 * 13, 14 * 15,16 * 17,18 * 19,20 * 21};
+       int[] answerBank = {6,20,42,72,110,156,210,272,342,420};
 
         for(int i=0;i<5;i++){
            int res =  rndm.nextInt(1,10);
