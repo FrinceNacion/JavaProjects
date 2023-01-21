b856c6aedd1d3f9fe03d57333d42045ca3591289
MagentaMagenta <119783546+FrinceNacion@users.noreply.github.com>
2023-01-21 13:18:00 +0800

    Update CompoundInterestCalculator.java

--- CompoundInterestCalculator.java
+++ CompoundInterestCalculator.java
@@ -8,22 +8,25 @@ public class CompoundInterestCalculator{
             int p = input.nextInt();
             System.out.println("Enter annual rate: ");
             double r = input.nextDouble();
+            
             System.out.println("Enter compund periods: "+"\n"+
-            "1 if compounded annualy"+"\n"+
-            "2 if compounded semi-annualy"+"\n"+
-            "4 if compounded quarterly"+"\n"+
-            "12 if compounded monthly"+"\n"+
-            "365 if compoundeddaily");
+                "1 if compounded annualy"+"\n"+
+                "2 if compounded semi-annualy"+"\n"+
+                "4 if compounded quarterly"+"\n"+
+                "12 if compounded monthly"+"\n"+
+                "365 if compoundeddaily");
+            
             int m = input.nextInt();
             System.out.println("Enter the number of years: ");
             int y = input.nextInt();
             int n = m*y;
 
             System.out.println("Given values: "+"\n"+
-            "Principal value: "+p+"\n"+
-            "Annual rate: "+r+"\n"+
-            "Compound period: "+m+"\n"+
-            "Total number of compound period: "+n);
+                "Principal value: "+p+"\n"+
+                "Annual rate: "+r+"\n"+
+                "Compound period: "+m+"\n"+
+                "Total number of compound period: "+n);
+            
             /*Compound Amount */
             double st = r/m + 1;
             double nd = Math.pow(st, n);
@@ -32,9 +35,8 @@ public class CompoundInterestCalculator{
             /*Compound Interest */
             double th = rd-p;
             
-
             System.out.println("You will have "+rd+" at the end of "+y+" years"+"\n"+
-            "and the compoud interest is "+th);
+                "and the compoud interest is "+th);
         }
         catch (InputMismatchException e) {
             System.out.println("Please enter an integer");
